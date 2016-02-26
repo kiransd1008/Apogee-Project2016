53a009182edb831a5372b5fd8fa5a1c695eea254
KIRAN SHANTINATH DHAMANE <kiransd1008@gmail.com>
2016-02-27 03:39:37 +0530

    Update final transmitter code.ino

--- final transmitter code.ino
+++ final transmitter code.ino
@@ -1,53 +1,62 @@
+int D3=7,D2=6,D1=5,D0=4;
+
+int ModuleVcc=;
 int sensorPin = A2;
-int motorPin = 12;
-int requiredLevel = 0;
-int waterLevel = 1023;    //zero in water and 1023 in Dry Soil
-int pushButton = A5;
+
+//int waterLevel = 1023;    //zero in water and 1023 in Dry Soil
+int waterPercentage=0;    
 int sensorVcc = A0;
-int buttonState;
 
 void setup() {
   Serial.begin(9600);
   pinMode(sensorVcc, OUTPUT);
   pinMode(sensorPin, INPUT);
-  pinMode(motorPin, OUTPUT);
-  pinMode(pushButton, INPUT);
-  for (int i = 2; i < 12; i++)
-    pinMode(i, OUTPUT);
   digitalWrite(sensorVcc, LOW);
-
-  int count = 0;
-  delay(2000);
-  buttonState = analogRead(pushButton);
-
-  delay(1000);
-  int p = 1;
-  while (p)
-  {
-    p = 0;
-    delay(500);
-    buttonState = analogRead(pushButton);
-    if (buttonState > 1000)
-    {
-      for (int i = 2; i < 12; i++)
-      {
-        digitalWrite(i, HIGH);
-        delay(500);
-        requiredLevel = 1000 - (100 * (i - 1));
-        if (analogRead(pushButton) < 1000)
-        {
-          break;
-          delay(2000);
-        }
-      }
-    }
-  }
-  delay(5000);
-  for (int i = 2; i < 12; i++)
-    digitalWrite(i, LOW);
+for(int count=0;count<4;count++)
+{
+  pinMode(count+4,OUTPUT);
+  digitalWrite(count+4,LOW);
+}
+  
 }
 
 void loop() {
+  for(int count=0;count<4;count++)
+     digitalWrite(count+4,LOW);
+  digitalWrite(sensorVcc,HIGH);
+  
+  Serial.println(analogRead(sensorPin));
+  
+  
+  if(analogRead(sensorPin)>1000)
+    waterPercentage=4;
+  else if(analogRead(sensorPin)>1023*0.75)
+    waterPercentage=3;
+  else if(analogRead(sensorPin)>1023*0.5)
+    waterPercentage=2;
+  else if(analogRead(sensorPin)>1023*0.25)
+    waterPercentage=1;
+  else (analogRead(sensorPin)>0)
+    waterPercentage=0;
+
+if(waterPercentage!=0)
+{
+  for(int count=0;count<waterPercentage;count++)
+      digitalWrite(count+4,HIGH);
+}      
+else 
+{
+  int i=0;
+  while(i<3)
+  {
+    for(int count=0;count<waterPercentage;count++)
+      digitalWrite(count+4,HIGH);
+    delay(1000);
+    for(int count=0;count<waterPercentage;count++)
+      digitalWrite(count+4,LOW);
+      i++;
+  }
+}
   Serial.print("RequiredLevel :");
   Serial.println(requiredLevel);
   digitalWrite(sensorVcc, HIGH);
