610e59fcc3a4b3ce0e41e5aadbab897b593a32da
KIRAN SHANTINATH DHAMANE <kiransd1008@gmail.com>
2016-02-27 07:47:53 +0530

    Update final transmitter code.ino

--- final transmitter code.ino
+++ final transmitter code.ino
@@ -1,10 +1,10 @@
 int D3=7,D2=6,D1=5,D0=4;
 
-int ModuleVcc;
+int ModuleVcc=A5;
 int sensorPin = A2;
 
 //int waterLevel = 1023;    //zero in water and 1023 in Dry Soil
-int waterPercentage=0;    
+int waterLevel=0;    
 int sensorVcc = A0;
 
 void setup() {
@@ -20,49 +20,34 @@ for(int count=0;count<4;count++)
   digitalWrite(count+4,LOW);
 }
   
-Serial.print("reciever module is ON");
+Serial.print("transmitter module is ON");
 }
 
-void loop() {
-  
-  digitalWrite(sensorVcc,HIGH);
-  Serial.print("MoistureSensor is on");
-  
-  Serial.println("SensorReading = ");
-  Serial.print(analogRead(sensorPin));
-
-  if(analogRead(sensorPin)>1000)
-    waterPercentage=4;
-  else if(analogRead(sensorPin)>1023*0.75)
-    waterPercentage=3;
-  else if(analogRead(sensorPin)>1023*0.5)
-    waterPercentage=2;
-  else if(analogRead(sensorPin)>1023*0.25)
-    waterPercentage=1;
-  else (analogRead(sensorPin)>0)
-    waterPercentage=0;
-
-Serial.println("MoistureSesor is OFF");
-digitalWrite(sensorVcc,LOW);
-  
-//delay(2000);
-if(waterPercentage!=0)
+void loop() 
 {
-  for(int count=0;count<waterPercentage;count++)
-      digitalWrite(count+4,HIGH);
-}      
-else 
-{
-  int i=0;
-  while(i<3)
-  {
-    for(int count=0;count<waterPercentage;count++)
-      digitalWrite(count+4,HIGH);
-    delay(1000);
-    for(int count=0;count<waterPercentage;count++)
-      digitalWrite(count+4,LOW);
-      i++;
-  }
+      digitalWrite(sensorVcc,HIGH);
+      Serial.print("MoistureSensor is on");
+      
+      Serial.println("SensorReading = ");
+      Serial.print(analogRead(sensorPin));
+      int sensorReading=analogRead(sensorPin);
+      
+      digitalWrite(sensorVcc,LOW);
+      Serial.print("MoistureSensor is OFF");
+      
+      if(sensorReading>1023*0.75)
+        waterLevel=4;
+      else if(sensorReading>1023*0.5)
+        waterLevel=3;
+      else if(sensorReading>1023*0.25)
+        waterLevel=2;
+      else
+        waterLevel=1;
+    
+      
+    if(waterLevel!=0)
+      for(int count=0;count<waterLevel;count++)
+          digitalWrite(count+4,HIGH);
 }
  delay(5000); 
 for(int count=0;count<4;count++)
