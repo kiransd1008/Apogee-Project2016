6188e7e1518cc34df12db3b2413a43af3ba9593f
KIRAN SHANTINATH DHAMANE <kiransd1008@gmail.com>
2016-02-27 06:14:09 +0530

    Update final receiver code.ino

--- final receiver code.ino
+++ final receiver code.ino
@@ -1,1 +1,96 @@
+int D0=4,D1=5,D2=6,D3=7;
+int RxModuleVcc;
+int motorPin = 12;
+int waterLevel = 1023;    //zero in water and 1023 in Dry Soil
+int ReceivingSignal=8;
+int PushButton=13;
+int ReferenceLevel;
+int Signal;
+void setup() {
+  Serial.begin(9600);
+  pinMode(RxModuleVcc,OUTPUT);
+  digitalWrite(RxModuleVcc,LOW);
+  
+  pinMode(ReceivingSignal, INPUT);
+  pinMode(motorPin, OUTPUT);
+  digitalWrite(motorPin,LOW);
+  for (int i = 4; i < 8; i++)
+    {
+      pinMode(i, OUTPUT);
+      digitalWrite(i,LOW);
+    }
+  Serial.println("set reference level");
+  Serial.println("push the push button for changing the waterlevel by 25 percent");
+  Serial.printl("Tap the push button for continuously 3 seconds to save the Reference Waterlevel");
+   while(digitalRead(PushButton)==0);
+   ReferenceLevel=setReferenceWaterLevel();
+    Serial.print("reference Level is ");  
+    Serial.println(ReferenceLevel);
+  for (int i = 4; i < 8; i++)
+      pinMode(i, INPUT);
+  //while(ReceivingSignal()==0);
+  
+}
 
+void loop() {
+  digitalWrite(RxModuleVcc, HIGH);
+  Serial.println("RxModule is turned ON");
+  Signal=ReadSignal();
+  Serial.println(Signal);
+  display(Signal);
+  digitalWrite(RxModuleVcc,LOW);
+  Serial.println("RxModule is turned OFF");
+
+  if (Signal < referenceLevel) {
+    digitalWrite(motorPin, HIGH);
+    Serial.println("Motor has started");
+  }
+  else
+    digitalWrite(motorPin, LOW);
+  delay(2 * 60 * 1000);
+}
+
+void display(int leds){
+  leds=leds%5;
+  for(int count=0;count+4<8;count++){
+    digitalWrite(count+4,LOW);
+  }
+  for(int count=0;count<leds;count++){
+    digitalWrite(count+4,HIGH);
+  }
+  return;
+}
+int ReadSignal(){
+  while(digitalRead(ReceivingSigal)==0);
+  int level=0;
+  for(int i=0;i<4;i++)
+    level+=digitalRead(i+4);
+  return level;
+}
+int setReferenceWaterLevel(){
+  int counter=0,count1=0,ButtonState=0,LastButtonState=0;
+  while(count1<=6)
+    {
+        ButtonState=digitalRead(PushButton);
+      if(LastButtonState!=ButtonState)
+        {
+          count1=0;
+          counter++;
+          display(counter);
+          delay(400);
+        }
+      else if(ButtonState)
+        {
+          count1++;
+          delay(400);
+        }
+      lastButtonState=ButtonState;
+    }
+    //display(counter);
+    delay(500);
+    display(0);
+    display(counter);
+    delay(500);
+    display(0);
+    return (counter%5);
+}
